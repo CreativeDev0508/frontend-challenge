89426787adbf170a891a3785b1f0e3b166b5319e
Luckyman909 <mingw2816@gmail.com>
2021-05-30 23:35:51 -0300

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <Logo />
+      <h1 class="text-5xl text-teal-700 font-semibold">
+        coodesh-frontend-challenge
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
