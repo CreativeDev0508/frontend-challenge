d92a1c13d6e4c0291cd8080ac86e92eb5f174453
Luckyman909 <mingw2816@gmail.com>
2021-05-31 16:58:15 -0300

    feat: add header component

---
+++ windi.config.js
@@ -0,0 +1,19 @@
+export default {
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          500: '#32286F',
+        },
+        secondary: {
+          300: '#9AE7C2',
+          500: '#21F3B4',
+        },
+      },
+    },
+  },
+  shortcuts: {},
+}
