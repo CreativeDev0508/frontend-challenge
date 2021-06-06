a2ef9f0ad66af69cb3722bfafe657a21b9a4c98c
Luckyman909 <mingw2816@gmail.com>
2021-06-05 22:59:00 -0300

    feat: add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div class="mt-10 mb-6">
+      <h1 v-if="error.statusCode === 404" class="text-2xl">
+        Ops... Page not found
+      </h1>
+      <h1 v-else>An error occurred</h1>
+    </div>
+    <NuxtLink to="/" class="btn">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
